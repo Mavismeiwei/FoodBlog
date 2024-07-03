263483d701a4e1e8598f0f5907d7c578101c7213
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-07-03 18:38:25 -0400

    Article like and collect functions

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     re_path('article/(?P<nid>\d+)/', article.ArticleView.as_view()),  # 编辑文章
     re_path('article/comment/(?P<nid>\d+)/', comment.CommentView.as_view()),  # 发布评论
     re_path('comment/digg/(?P<nid>\d+)/', comment.CommentDiggView.as_view()),  # 评论点赞
+
+    re_path('article/digg/(?P<nid>\d+)/', article.ArticleDiggView.as_view()),  # 文章点赞
+    re_path('article/collects/(?P<nid>\d+)/', article.ArticleCollectsView.as_view()),  # 文章收藏
 ]
