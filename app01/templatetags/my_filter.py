b7ed679f365f6170e0f744e18182cc57a08b26a3
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-07-04 09:02:06 -0400

    Add a filter to keep the collection status when refresh the browser.

---
+++ app01/templatetags/my_filter.py
@@ -0,0 +1,14 @@
+from django import template
+
+# 注册
+register = template.Library()
+
+# 判断用户是否收藏了文章
+@register.filter
+def is_user_collects(article, request):
+    # 用户登录判断
+    if str(request.user) == 'AnonymousUser':  # 用户未登录的话
+        return ''
+    if article in request.user.collects.all():  # 文章收藏的话 return True
+        return 'show'  # 已收藏的话那就返回show这个class
+    return ''  # 否则返回空
