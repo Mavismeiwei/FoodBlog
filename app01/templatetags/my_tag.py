ba8d2e33ac891aa8d4759d6368ef4c2c8e3baebc
Mavismeiwei <zhang.meiwe@northeastern.edu>
2024-07-22 11:20:56 -0400

    Update dynamic shuffle function

--- app01/templatetags/my_tag.py
+++ app01/templatetags/my_tag.py
@@ -1,6 +1,6 @@
 from django import template
 import re
-from app01.models import Tags, Avatars
+from app01.models import Tags, Avatars, Menu
 from app01.utils.search import Search
 from django.utils.safestring import mark_safe
 
@@ -9,17 +9,31 @@ register = template.Library()
 
 @register.inclusion_tag('my_tag/headers.html')
 def banner(menu_name, article=None):
-    img_list = [
-        "/static/resources/img/home/bg1.jpeg",
-        "/static/resources/img/home/bg2.jpeg",
-        "/static/resources/img/home/bg7.jpeg"
-    ]
-    if article:  # check if current page is the article detail page
-        # get the article cover
+    if article:
+        # 说明是文章详情页面
+        # 拿到文章的封面
         cover = article.cover.url.url
         img_list = [cover]
-        pass
-    return {"img_list": img_list}
+        title = article.title
+        slogan_list = [article.abstract[:30]]
+        return locals()
+
+    menu_obj = Menu.objects.get(menu_title_en=menu_name)
+    img_list = [i.url.url for i in menu_obj.menu_url.all()]
+    menu_time = menu_obj.menu_time
+    title = menu_obj.title
+    slogan_list = menu_obj.abstract.replace('；', ';').replace('\n', ';').split(';')
+    slogan_time = menu_obj.abstract_time
+    if not menu_obj.menu_rotation:
+        # 如果不轮播
+        img_list = img_list[0:1]
+        menu_time = 0
+
+    if not menu_obj.rotation:
+        slogan_list = slogan_list[0:1]
+        slogan_time = 0
+
+    return locals()
 
 @register.simple_tag
 # 搜索页过滤器
